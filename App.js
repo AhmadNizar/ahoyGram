9b3d44ada16167c3fc41363d57c06e360fca073b
chandrabuwana <cbuwana@gmail.com>
2018-01-05 23:55:32 +0700

    add react-redux

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import store from './store'
 import { Text } from 'react-native'
 import MainStack from './components/cloneMain'
-
+import { Provider } from 'react-redux'
 export default class App extends React.Component {
   render() {
     return (
       // <Text>AhmadNizar</Text>
-      <MainStack/>
+      <Provider store ={store}>
+        <MainStack/>      
+      </Provider>
     );
   }
 }
