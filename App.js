318b223c2331f2b87e3d62cd375ac7a960eaa4be
AhmadNizar <ahmadnizar.owl@gmail.com>
2018-01-05 23:16:05 +0700

    -add dummy

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import store from './store'
 import { Text } from 'react-native'
-// import MainStack from './ahoyGram/components/main'
+import MainStack from './components/cloneMain'
 
 export default class App extends React.Component {
   render() {
     return (
-      <Text>AhmadNizar</Text>
-      // <MainStack/>
+      // <Text>AhmadNizar</Text>
+      <MainStack/>
     );
   }
 }
